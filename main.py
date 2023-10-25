9872652027da1fd7e21a436697eafc468be8ad07
Bully-Boy <roma.voronov.voronov@gmail.com>
2023-10-26 01:25:34 +0300

    edit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 
 if __name__ == '__main__':
-    tracker = Tracker(show=False, video_path='data/videos/hay_v1_fhd.mp4', conf=0.1, imgsz=Size.s4K)
+    tracker = Tracker(show=True, video_path='data/videos/hay_v1_fhd.mp4', conf=0.1, imgsz=Size.s4K)
     tracker.process_video()
     # tracker.save_video()
     # predictor = Predictor(show=True, video_path='data/videos/hay_v1_fhd.mp4', conf=0.1, imgsz=Size.s1K)
