376246412a6fc09d8ce8b11470be0d677ecdfe54
artemii <roke.berezin@gmail.com>
2023-10-25 20:32:50 +0300

    pep8

--- handler/track.py
+++ handler/track.py
@@ -6,7 +6,8 @@ import cv2
 class Tracker(HandlerBase):
 
     def annotate_frame(self, frame: cv2.typing.MatLike) -> cv2.typing.MatLike:
-        results = self.model.track(frame, persist=True, conf=getattr(self, 'conf', 0.2), imgsz=getattr(self, 'imgsz', 640))
+        results = self.model.track(frame, persist=True, conf=getattr(self, 'conf', 0.2),
+                                   imgsz=getattr(self, 'imgsz', 640))
         try:
             self.counter.update(results[0].boxes.id.int().cpu().tolist())
             if self.hide_labels:
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from handler.track import Tracker
 from handler.predict import Predictor
+import os
 
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 if __name__ == '__main__':
-    tracker = Tracker(show=False, conf=0.1, imgsz=(1920, 1080))
+    tracker = Tracker(show=True, conf=0.1, imgsz=(1920, 1080))
     tracker.process_video()
     # predictor = Predictor(show=True, conf=0.15, imgsz=(1920, 1056))
     # predictor.process_video()
-
